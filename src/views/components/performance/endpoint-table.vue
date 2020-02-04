4cc2fe002cb1d19be87b50f6cb10b984fb111956
“叶尐楠” <xyezir@gmail.com>
2020-02-04 18:17:28 +0800

    Adjust field display

--- src/views/components/performance/endpoint-table.vue
+++ src/views/components/performance/endpoint-table.vue
@@ -1,5 +1,5 @@
 <template>
-  <rk-table :data="performances" :columns="columns" bordered :scroll="{ x: 2400 }"></rk-table>
+  <rk-table :data="data" :columns="columns" bordered :scroll="{ x: 2400 }"></rk-table>
 </template>
 
 <script lang="ts">
@@ -24,8 +24,6 @@
         key: 'name',
         title: 'name',
         width: 500,
-        titleAlign: 'center',
-        columnAlign: 'center',
         ellipsis: true,
         sorter: true,
       },
@@ -33,105 +31,79 @@
         key: 'timeBucket',
         title: 'time',
         width: 120,
-        titleAlign: 'center',
-        columnAlign: 'center',
       },
       {
         key: 'percentage',
-        title: 'percentage(%)',
+        title: 'SLA(%)',
         width: 140,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'total',
         title: 'total',
         width: 120,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
-        key: 'match',
-        title: 'match',
+        key: 'fail',
+        title: 'fail',
         width: 120,
-        titleAlign: 'center',
-        columnAlign: 'center',
-        sorter: true,
-      },
-      {
-        key: 'count',
-        title: 'count',
-        width: 120,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'cpm',
         title: 'cpm',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'max',
         title: 'max(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'min',
         title: 'min(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'avg',
         title: 'avg(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'p50',
         title: 'p50(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'p75',
         title: 'p75(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'p90',
         title: 'p90(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'p99',
         title: 'p99(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
     ];
+
+    private data: any[] = this.performances.map((pfm: any) => {
+      pfm.fail = pfm.count - pfm.match;
+      pfm.percentage = (pfm.percentage / 100).toFixed(2);
+      return pfm;
+    });
   }
 </script>
--- src/views/components/performance/instance-table.vue
+++ src/views/components/performance/instance-table.vue
@@ -1,5 +1,5 @@
 <template>
-  <rk-table :data="performances" :columns="columns" bordered :scroll="{ x: 2200 }"></rk-table>
+  <rk-table :data="data" :columns="columns" bordered :scroll="{ x: 2200 }"></rk-table>
 </template>
 
 <script lang="ts">
@@ -41,106 +41,80 @@
         key: 'timeBucket',
         title: 'time',
         width: 120,
-        titleAlign: 'center',
-        columnAlign: 'center',
         ellipsis: true,
       },
       {
         key: 'percentage',
-        title: 'percentage(%)',
+        title: 'SLA(%)',
         width: 140,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'total',
         title: 'total',
         width: 120,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
-        key: 'count',
-        title: 'count',
+        key: 'fail',
+        title: 'fail',
         width: 120,
-        titleAlign: 'center',
-        columnAlign: 'center',
-        sorter: true,
-      },
-      {
-        key: 'match',
-        title: 'match',
-        width: 120,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'cpm',
         title: 'cpm',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'max',
         title: 'max(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'min',
         title: 'min(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'avg',
         title: 'avg(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'p50',
         title: 'p50(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'p75',
         title: 'p75(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'p90',
         title: 'p90(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
       {
         key: 'p99',
         title: 'p99(ms)',
         width: 100,
-        titleAlign: 'center',
-        columnAlign: 'center',
         sorter: true,
       },
     ];
+
+    private data: any[] = this.performances.map((pfm: any) => {
+      pfm.fail = pfm.count - pfm.match;
+      pfm.percentage = (pfm.percentage / 100).toFixed(2);
+      return pfm;
+    });
   }
 </script>
